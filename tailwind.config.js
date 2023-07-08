b875c8c9365fc2064fbd55762d6c44a081526180
izzat5233 <izzat5233@gmail.com>
2023-07-08 14:57:52 +0300

    Added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'sans': ['Droid Arabic Kufi', 'serif'],
+            },
+            colors: {
+                'primary': '#FF8A71',
+                'secondary': '#F49867',
+                'accent': '#AE67FA',
+                'back': '#fff',
+                'fore': '#040C18',
+                'fore-neutral': '#031B34',
+                'fore-light': '#81AFDD'
+            },
+        },
     },
+    variants: {},
     plugins: [],
 }
