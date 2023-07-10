827a62355af7dab408e1fb910a9ae66925f0d547
TaimaHamadneh <taimanizar45@gmail.com>
2023-07-10 19:45:06 +0300

    Update home page

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,77 +1,77 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import storage from '../../assets/storage.png';
 import sys from '../../assets/management_system.png';
 import Partial from '../../assets/Partial_Storage.png';
 import connection from '../../assets/connection.png';
 import time from '../../assets/time_storage.png';
 import './home.css';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 
 const Home = () => {
-  const cardsData = [
-    {
-      imgSrc: sys,
-      title: "إدارة المخازن",
-      description: "تتمثل هذه الجزئية بتوفير خدمات ادارية وخدمات اشراف على المخازن"
-    },
-    {
-      imgSrc: Partial,
-      title: "تخزين مؤقت",
-      description: "تتمثل هذه الخدمة بتوفير مساحات للافراد والعائلات لتخزين معدات او ادوات لا يتم استخدامها لفترة معينة."
-    },
-    {
-      imgSrc: connection,
-      title: "الربط بين المؤجر والمستأجر",
-      description: "تتمثل الخدمة بتوفير مساحة الكترونية تسمح للمؤجر بعرض مخزنه ومتطلباته لتأجير هذا المخزن"
-    },
-    {
-      imgSrc: time,
-      title: "تخزين جزئي",
-      description: "تتمثل هذه الخدمة بمساعدة كل من المؤجرين والمستأجرين باستغلال المساحات التخزينية المتوفرة بما بتناسب مع حاجة المستأجر ومصلحة المؤجر"
-    }
-  ];
-  const features = [
-    {
-      title: 'تخزين فعال',
-      description: 'نساعدك في تنظيم المساحة وترتيب البضائع بطريقة تسمح بتخزين أكبر عدد من السلع في مساحة محدودة.',
-    },
-    {
-      title: 'توفير الوقت والجهد',
-      description: '',
-    },
-    {
-      title: 'الأمان والحماية ',
-      description: ''
-    },
-    {
-      title: 'إدارة المخزون',
-      description: '',
-    },
-  ];
-  return (
-    <div>
-      <div>
-        <div className="header section__padding" id="Home">
-          <div className="header-content" style={{ textAlign: "right" }}>
-            <h1 className="gradient__text"> مرحبًا بك في شركة تخزين!</h1>
-            <p>  احصل على حلاً مبتكرًا وفعالًا لتحقيق أهدافك المالية من خلال إدارتنا الفعالة لمستودعاتك .</p>
+    const cardsData = [
+        {
+          imgSrc: sys,
+          title: "إدارة المخازن",
+          description: "تتمثل هذه الجزئية بتوفير خدمات ادارية وخدمات اشراف على المخازن"
+        },
+        {
+          imgSrc: Partial,
+          title: "تخزين مؤقت",
+          description: "تتمثل هذه الخدمة بتوفير مساحات للافراد والعائلات لتخزين معدات او ادوات لا يتم استخدامها لفترة معينة."
+        },
+        {
+          imgSrc: connection,
+          title: "الربط بين المؤجر والمستأجر",
+          description: "تتمثل الخدمة بتوفير مساحة الكترونية تسمح للمؤجر بعرض مخزنه ومتطلباته لتأجير هذا المخزن"
+        },
+        {
+          imgSrc: time,
+          title: "تخزين جزئي",
+          description: "تتمثل هذه الخدمة بمساعدة كل من المؤجرين والمستأجرين باستغلال المساحات التخزينية المتوفرة بما بتناسب مع حاجة المستأجر ومصلحة المؤجر"
+        }
+      ];
+      const features = [
+        {
+          title: 'تخزين فعال',
+          description: 'نساعدك في تنظيم المساحة وترتيب البضائع بطريقة تسمح بتخزين أكبر عدد من السلع في مساحة محدودة.',
+        },
+        {
+          title: 'توفير الوقت والجهد',
+          description: '',
+        },
+        {
+          title: 'الأمان والحماية ',
+          description: ''
+        },
+        {
+          title: 'إدارة المخزون',
+          description: '',
+        },
+      ];
+    return (
+        <div>
+            <div>
+                <div className="header section__padding" id="Home">
+                    <div className="header-content" style={{textAlign: "right"}}>
+                        <h1 className="gradient__text"> مرحبًا بك في شركة تخزين!</h1>
+                        <p> احصل على حلٍ مبتكر لتحقيق أهدافك المالية من خلال إدارتنا الفعالة لمستودعاتك .</p>
 
-            <div className="header-content__input">
+                        <div className="header-content__input">
 
-              <button type="button"><Link to="/System">إدارة المخازن</Link></button>
-              <button type="button"><Link to="/Owner">إعلان لمخزنك</Link> </button>
-              <button type="button"><Link to="/Search">البحث عن مخزن</Link> </button>
+                            <button type="button"><Link to="/management">إدارة المخازن</Link></button>
+                            <button type="button"><Link to="/owner">إعلان لمخزنك</Link></button>
+                            <button type="button"><Link to="/search">البحث عن مخزن</Link></button>
+                        </div>
+                    </div>
+
+                    <div className="header-image">
+                        <img src={storage} alt="Storage Logo"/>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <div className="header-image">
-            <img src={storage} />
-          </div>
-        </div>
-      </div>
-      <div className='space'></div>
-      <section className="cards" id="services">
+            <div className='space'></div>
+            <section className="cards" id="services">
         <h1 className="title">خـــدمـــاتـــنـــا</h1>
         <div className="content">
           {cardsData.map((card, index) => (
@@ -108,8 +108,8 @@ const Home = () => {
 
 
     </div>
-  )
-  
+    )
 }
 
 export default Home
+
