9f06b30d210b7af302ada4757dec0022bccb9b91
izzat5233 <izzat5233@gmail.com>
2023-07-12 00:30:01 +0300

    Added basic PartialForm

--- src/component/Pages/Find/Stage/PartialForm.jsx
+++ src/component/Pages/Find/Stage/PartialForm.jsx
@@ -1,7 +1,49 @@
-import ComingSoon from "../../Misc/ComingSoon/ComingSoon";
+import Form from "../../../Util/Form/Form";
+import Field from "../../../Util/Form/Field";
 
 export default function PartialForm() {
     return (
-        <ComingSoon/>
+        <div dir="rtl" className="
+            min-h-screen pt-24
+            flex justify-center
+            bg-gray-100
+        ">
+            <Form
+                className="h-fit"
+                title="المعلومات الأساسية"
+                onSubmit={() => {
+                }}
+            >
+                <Field
+                    label="الاسم"
+                    type="text"
+                />
+                <Field
+                    label="رقم الهاتف"
+                    type="tel"
+                />
+                <Field
+                    label="مكان السكن"
+                    type="text"
+                />
+                <hr className="my-8"/>
+                <Field
+                    label="نوع المواد المراد تخزينها"
+                    type="text"
+                />
+                <Field
+                    label="المدة المتوقعة للاستئجار"
+                    type="number"
+                />
+                <Field
+                    label="المساحة المراد استئجارها"
+                    type="number"
+                />
+                <Field
+                    label="المواقع المرغوبة للمخزن"
+                    type="text"
+                />
+            </Form>
+        </div>
     );
 }
--- src/component/Util/Form/Form.jsx
+++ src/component/Util/Form/Form.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function Form({title, onSubmit, children}) {
+export default function Form({title, onSubmit, children, className = ""}) {
     return (
-        <div className="
+        <div className={`
             p-10 flex flex-col gap-4
             bg-white drop-shadow-2xl rounded-md
             transition-all duration-150
-        ">
+            ${className}
+        `}>
             {title && <h2 className="text-center text-4xl mb-8">{title}</h2>}
             <form onSubmit={(event) => {
                 event.preventDefault();
