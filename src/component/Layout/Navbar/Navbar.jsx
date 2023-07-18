6c147b2da99e269d0fac29cd9f4f5a99b82399a8
izzat5233 <izzat5233@gmail.com>
2023-07-18 22:27:35 +0300

    Updated navbar

--- src/component/Layout/Navbar/Navbar.jsx
+++ src/component/Layout/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
-import logo from '../../../assets/icons/normal/logoWithBorder.png';
+import React, {useEffect, useState} from 'react'
+import logo from '../../../assets/icons/normal/logo.png';
+import logoWithText from '../../../assets/icons/normal/logoWithText.png';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import {Link} from 'react-router-dom';
 import {motion, useMotionValueEvent, useScroll} from "framer-motion";
@@ -9,6 +10,17 @@ export default function Navbar() {
     const {scrollY} = useScroll();
     const [prevScrollY, setPrevScrollY] = useState(0);
     const [hidden, setHidden] = useState(false);
+    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1024); // lg
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsWideScreen(window.innerWidth > 1024);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     useMotionValueEvent(scrollY, "change", (latest) => {
         setHidden(latest > prevScrollY);
@@ -24,18 +36,22 @@ export default function Navbar() {
             animate={hidden ? "hidden" : "visible"}
             transition={{ease: [0.1, 0.25, 0.3, 1], duration: 0.6}}
             className="
-                bg-transparent z-20 flex
+                bg-transparent z-20 flex lg:flex-row-reverse lg:justify-start
                 fixed top-0 left-0 right-0
                 justify-between justify-items-center
                 py-4 px-4 md:px-8
             "
         >
-            <img src={logo} alt="logo" className="w-auto h-10 justify-start"/>
-            <div className="
-                hidden lg:flex flex-row-reverse justify-end
-                pr-8 justify-items-center
-            ">
-                <NavbarSignLinks/>
+            <img src={isWideScreen ? logoWithText : logo} alt="logo"
+                 className={`justify-start w-auto ${isWideScreen ? "h-16" : "h-10"}`}/>
+            <div className="hidden lg:flex flex-col justify-center">
+                <div className="
+                    flex flex-row-reverse justify-end
+                    pr-8 justify-items-center
+                ">
+                    <NavbarSignLinks/>
+                </div>
+
             </div>
             <div className="
                 ml-4 flex lg:hidden relative my-auto
@@ -93,7 +109,7 @@ function NavbarSignLink({title, to}) {
         ">
             <Link to={to} className="
                 before:content-{}
-                before:absolute before:-bottom-0 before:left-0 before:w-full before:h-0.5
+                before:absolute before:-bottom-1 before:left-0 before:w-full before:h-0.5
                 before:bg-strong before:text-back
                 before:scale-x-0 before:origin-left before:transition before:duration-200 before:ease-out
                 hover:before:scale-x-100
