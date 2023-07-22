afc62094181ece75c0896803cca3a0ff80af64b2
izzat5233 <izzat5233@gmail.com>
2023-07-22 22:22:22 +0300

    Added area, location, and price filters.

--- src/component/Pages/Find/Stage/Storage.jsx
+++ src/component/Pages/Find/Stage/Storage.jsx
@@ -3,18 +3,72 @@ import Page from "../../../Util/Page/Page";
 import FormButton from "../../../Util/Button/FormButton";
 import {BsPlus} from "react-icons/bs";
 import {ExpandedInOutBackground} from "../../../Util/Page/Background";
+import {BiSolidDownArrow} from "react-icons/bi";
+import {useEffect, useState} from "react";
 
 export default function Storage() {
     const [filteredData, filters, setFilters] = useFilters(storages);
+    const [areaChoice, setAreaChoice] = useState(null);
+    const [locationChoice, setLocationChoice] = useState(null);
+    const [priceChoice, setPriceChoice] = useState(null);
+
+    useEffect(() => {
+        let filter = {}
+        if (areaChoice && (areaChoice !== "الكل")) {
+            let range = areaChoice.split("-");
+            filter.size = (size) => (Number(range[0]) <= size && size <= Number(range[1]));
+        }
+        if (locationChoice && (locationChoice !== "الكل")) {
+            filter.location = (location) => locationChoice === location;
+        }
+        if (priceChoice && (priceChoice !== "الكل")) {
+            let range = priceChoice.split("-");
+            filter.price = (price) => (Number(range[0]) <= price && price <= Number(range[1]));
+        }
+        setFilters(filter);
+    }, [areaChoice, locationChoice, priceChoice]);
 
     return (
         <Page background={<ExpandedInOutBackground/>}>
             <div className="flex flex-col justify-center p-8">
                 <div className="flex flex-wrap justify-center gap-8 mb-8">
                     <p className="text-xl py-2">البحث من خلال:</p>
-                    <FilterButton label="المساحة"/>
-                    <FilterButton label="الموقع"/>
-                    <FilterButton label="السعر"/>
+                    <FilterButton
+                        title="المساحة"
+                        labels={[
+                            "الكل",
+                            "10 - 20",
+                            "20 - 40",
+                            "40 - 80",
+                            "80 - 100"
+                        ]}
+                        currentLabel={areaChoice}
+                        setCurrentLabel={setAreaChoice}
+                    />
+                    <FilterButton
+                        title="الموقع"
+                        labels={[
+                            "الكل",
+                            "نابلس",
+                            "رام الله",
+                            "طولكرم",
+                            "الخليل"
+                        ]}
+                        currentLabel={locationChoice}
+                        setCurrentLabel={setLocationChoice}
+                    />
+                    <FilterButton
+                        title="السعر"
+                        labels={[
+                            "الكل",
+                            "1000 - 2000",
+                            "2000 - 4000",
+                            "4000 - 8000",
+                            "8000 - 10000"
+                        ]}
+                        currentLabel={priceChoice}
+                        setCurrentLabel={setPriceChoice}
+                    />
                 </div>
                 <div className="flex flex-col flex-grow">
                     {filteredData.map((storage, index) => (
@@ -30,14 +84,34 @@ export default function Storage() {
     );
 }
 
-function FilterButton({label, choices}) {
+function FilterButton({title, labels, currentLabel, setCurrentLabel}) {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <button className="
-            py-2 px-4 rounded transition-all
-            text-lg lg:text-xl text-center
-            shadow-md bg-back hover:scale-105
+        <button
+            onClick={() => setExpanded(!expanded)}
+            className="
+                py-2 px-4 h-fit
+                flex flex-col justify-center
+                rounded transition-all
+                text-lg lg:text-xl text-center
+                shadow-md bg-back hover:scale-105
         ">
-            <p>{label}:&emsp;الكل</p>
+            <div className="flex gap-4">
+                <p>{title}{currentLabel && ":"}&emsp;{currentLabel}</p>
+                <BiSolidDownArrow className={`my-auto transition-transform duration-300 ${expanded && "rotate-180"}`}/>
+            </div>
+            {expanded &&
+                <div className="flex flex-col gap-1 mt-4 w-full">
+                    {labels && labels.map((label) => (
+                        <p
+                            onClick={() => setCurrentLabel(label)}
+                            className="hover:bg-gray-200 mx-auto py-2 w-full h-full"
+                        >
+                            {label}
+                        </p>
+                    ))}
+                </div>}
         </button>
     );
 }
@@ -47,7 +121,7 @@ function Card({key, storage}) {
         <div className="
             flex justify-around transition-all
             my-5 mx-5 p-4 rounded-3xl
-            bg-back shadow-xl hover:shadow-2xl
+            bg-back shadow-xl
         ">
             <img
                 src={storage.imageURL}
